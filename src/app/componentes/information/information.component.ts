f7f529be0fdeb65d0154e12a5adde80a1e37421a
AlejandroHdeez <jaandrohernandez@gmail.com>
2019-03-28 20:38:32 -0600

    Integracion de datos en pestaña Information

--- src/app/componentes/information/information.component.ts
+++ src/app/componentes/information/information.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-information',
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InformationComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
+  }
+
+  verificar(): void{
+    if(true){
+    this.router.navigate(["https://www.facebook.com/ShoesGuatemala/"])
+  }else{
+  
+  }
   }
 
 }
